297f6c990bb6d944fa3082b9c36e2df407d60c17
Tataru Robert <robert.tataru98@gmail.com>
2026-04-16 00:27:59 +0300

    Added TAFlags dependency

--- Package.swift
+++ Package.swift
@@ -43,6 +43,7 @@ let package = Package(
         .package(url: "git@github.com:TechArtists/ios-debug-tools.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "git@github.com:TechArtists/ios-swift-log-os-log-handler.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "git@github.com:TechArtists/ios-swift-log-file-log-handler.git", .upToNextMajor(from: "1.0.0")),
+        .package(url: "git@github.com:TechArtists/ios-flags.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "git@github.com:firebase/firebase-ios-sdk.git", .upToNextMajor(from: "12.0.0")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/superwall/Superwall-iOS.git", .upToNextMajor(from: "4.0.0")),
@@ -52,6 +53,7 @@ let package = Package(
         .target(
             name: packageName,
             dependencies: [
+                .product(name: "TAFlags", package: "ios-flags"),
                 .product(name: "TAAnalytics", package: "ios-analytics"),
                 .product(name: "TADebugTools", package: "ios-debug-tools"),
                 .product(name: "SwiftLogOSLogHandler", package: "ios-swift-log-os-log-handler"),
